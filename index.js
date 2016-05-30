68870580ebe2d66df32520c33a1a97ac4970c124
Justin Woolley <jwoolley@netflix.com>
2016-05-30 16:41:52 -0700

    promisfy mongodb connection

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 var mongodb = require('mongodb');
+var Promise = require('bluebird');
 var utils = require('./lib/utils');
 
 var MongoClient = mongodb.MongoClient;
 
-var config = {};
+var globals = {
+  config: {}
+};
 
-// TODO: parallelize loading of all configs via list (proise.all)
-loadConfig('mongo')
-  .then(() => {
-   
-
-    var url = 'mongodb://' + config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.dbName;
-
-    MongoClient.connect(url, function(err, db) {
-      if(err) {
-        console.log('Unable to connect to mongoDB server. Error: ' + err);
-      } else { 
-        console.log('Connection established to ' + url);
-        console.log('Bing bong, baby!');
-        db.close();
-      }
-    });    
+openMongo()
+  .then(connection => {
+    this.connection = connection;
+  })
+  .finally(() =>  {
+    this.connection.close(); 
   });
+
+
 
 function loadConfig(configName) {
   var configPath = './config/';
@@ -29,7 +24,31 @@ function loadConfig(configName) {
   var _config = configPath + configName + '.json';
   var _override = overridePath + configName + '.json';
 
+  if (globals.config.hasOwnProperty(configName)) {
+    return Promise.resolve(config[configName]);
+  }
+
   return utils.readFile(_override)
     .then(undefined, () => utils.readFile(_config))
-    .then(data => {  config[configName] = data; });
+    .then(data => {  globals.config[configName] = data; });
+}
+
+function openMongo() {
+  return new Promise(function(resolve, reject) {
+   return loadConfig('mongo')
+    .then(() => {   
+      var config = globals.config.mongo;
+
+      var url = 'mongodb://' + config.host + ':' + config.port + '/' + config.dbName;
+
+      MongoClient.connect(url, function(err, db) {
+        if(err) {
+          reject(new Error(err));
+        } else { 
+          console.log('Connection established to ' + url);
+          resolve(db);
+        }
+      });    
+    });
+  });
 }
